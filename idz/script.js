ab524d5bed570687151b7ff9ecf929cf3baa6d0a
Illia Chaparyn <illia.chaparyn@nixsolutions.com>
2019-12-09 19:04:11 +0200

    добавлено выравнивание ячеек

--- idz/script.js
+++ idz/script.js
@@ -9,27 +9,27 @@ var moveQueue = 'O';
 
 function clickHandler(el)
 {
-	if(noGame)
+	if( noGame )
 	{
 		cleanField();
-		noGame=false;
+		noGame = false;
 		setMoveText();
 		return;
 	}
-	el.innerHTML=userSymbol;
-	var cells = document.getElementsByTagName("td");
-	winChecker(el.cellIndex +(el.parentNode.rowIndex*3));
-	if(isWin==false)
+	el.innerHTML = userSymbol;
+	var cells = document.getElementsByTagName( "td" );
+	winChecker( el.cellIndex +( el.parentNode.rowIndex*3 ) );
+	if( isWin == false )
 	{
-		if(isPC==true)
+		if( isPC == true )
 		{
-		pcStep();
+			pcStep();
 		}
 		else
 		{
 			setMoveText();
-			if(userSymbol=='X') userSymbol='O';
-			else userSymbol='X';
+			if( userSymbol == 'X' ) userSymbol = 'O';
+			else userSymbol = 'X';
 		}
 	}
 	else
@@ -38,100 +38,97 @@ function clickHandler(el)
 	}
 }
 
-function setMoveText()
-{
-	var moveLabel=document.getElementById("move_label");
-	moveLabel.innerHTML = "move of " + moveQueue;
-	if(moveQueue=='X') moveQueue='O';
-	else moveQueue = 'X';
-}
-
 function formHandler()
 {
-	var isX = document.getElementById("symbolX").checked;
-	if(isX == false)
+	var isX = document.getElementById( "symbolX" ).checked;
+	if( !isX )
 	{
-		userSymbol='O';
-		pcSymbol='X';
+		userSymbol = 'O';
+		pcSymbol = 'X';
 	}
-	isPC = document.getElementById("playerPC").checked;
-	var field = document.getElementById("game_field");
-	field.style.display="block";
-	var dlg = document.getElementById("initial_dialog");
-	var moveLabel=document.getElementById("move_label");
+	isPC = document.getElementById( "playerPC" ).checked;
+	var field = document.getElementById( "game_field" );
+	field.style.display = "block";
+	var dlg = document.getElementById( "initial_dialog" );
+	var moveLabel=document.getElementById( "move_label" );
 	moveLabel.innerHTML = "move of " + userSymbol;
-	dlg.style.display="none";
+	dlg.style.display = "none";
 }
-function sleep(milliseconds) { 
-    let timeStart = new Date().getTime(); 
-    while (true) { 
-      let elapsedTime = new Date().getTime() - timeStart; 
-      if (elapsedTime > milliseconds) { 
-        break; 
-      } 
-    } 
-  } 
+
+function setMoveText()
+{
+	var moveLabel = document.getElementById("move_label");
+	moveLabel.innerHTML = "move of " + moveQueue;
+	if(moveQueue == 'X') moveQueue = 'O';
+	else moveQueue = 'X';
+}
   
 function winChecker(index)
 {
-var combinations=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-var cells = document.getElementsByTagName("td");
-for(var i=0;i<combinations.length;i++)
-{
-	var currentSymbol = document.getElementsByTagName("td")[index].innerHTML;
-	var isIndexInList = combinations[i].indexOf(index);
-	if(isIndexInList != -1)
+	var combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+	var cells = document.getElementsByTagName( "td" );
+	for( var i = 0;i < combinations.length; i++ )
 	{
-		var arr = combinations[i];
-		if(cells[arr[0]].innerHTML == currentSymbol && cells[arr[1]].innerHTML == currentSymbol && cells[arr[2]].innerHTML == currentSymbol)
+		var currentSymbol = document.getElementsByTagName( "td" )[index].innerHTML;
+		var isIndexInList = combinations[i].indexOf( index );
+		if(isIndexInList != -1)
 		{
-			doPaint(cells,combinations[i]);
-			noGame=true;
-			document.getElementById("move_label").innerHTML = currentSymbol+" has win";
-			moveQueue=userSymbol;
-			if(currentSymbol=='X') xWins++;
-			if(currentSymbol=='O') oWins++;
-			var counter = document.getElementById("count");
-			counter.innerHTML=String(xWins)+":"+String(oWins);
-			isWin=true;
-			return;
+			var arr = combinations[i];
+			if(cells[arr[0]].innerHTML == currentSymbol && cells[arr[1]].innerHTML == currentSymbol && cells[arr[2]].innerHTML == currentSymbol)
+			{
+				doPaint(cells,combinations[i]);
+				noGame = true;
+				document.getElementById( "move_label" ).innerHTML = currentSymbol + " has win";
+				moveQueue=userSymbol;
+				if( currentSymbol == 'X' ) xWins++;
+				if( currentSymbol == 'O' ) oWins++;
+				var counter = document.getElementById( "count" );
+				counter.innerHTML=String(xWins)+":"+String(oWins);
+				isWin = true;
+				return;
+			}
 		}
 	}
-}
-for(var i =0;i<cells.length;i++)
-	if(cells[i].innerHTML!='X' && cells[i].innerHTML!='O')
-		return;
-	noGame=true;
-	document.getElementById("move_label").innerHTML = "We are tied";
-			moveQueue=userSymbol;	
+	for( var i = 0; i < cells.length; i++ )
+		if( cells[i].innerHTML != 'X' && cells[i].innerHTML != 'O')
+		{
+			return;
+		}
+		noGame=true;
+		document.getElementById("move_label").innerHTML = "We are tied";
+		moveQueue = userSymbol;	
 }
 
 
 function doPaint(cells,combination)
 {
-	cells[combination[0]].style.backgroundColor="red";
-	cells[combination[1]].style.backgroundColor="red";
-	cells[combination[2]].style.backgroundColor="red";
+	cells[combination[0]].style.backgroundColor = "red";
+	cells[combination[1]].style.backgroundColor = "red";
+	cells[combination[2]].style.backgroundColor = "red";
 }
+
 function cleanField()
 {
 	var cells = document.getElementsByTagName("td");
-	for (var i = 0; i < cells.length; i++) {
-		cells[i].innerHTML="&emsp;"
-		cells[i].style.backgroundColor="white";
+	for ( var i = 0; i < cells.length; i++ ) 
+	{
+		cells[i].innerHTML = "&emsp;"
+		cells[i].style.backgroundColor = "white";
 	}
-	noGame=false;
+	noGame = false;
 }
+
 function pcStep()
 {
 	var cells = document.getElementsByTagName("td");
-	for (var i = 0; i < cells.length; i++) {
-		if(cells[i].innerHTML!=userSymbol && cells[i].innerHTML!=pcSymbol)
+	for ( var i = 0; i < cells.length; i++ ) 
+	{
+		if( cells[i].innerHTML != userSymbol && cells[i].innerHTML != pcSymbol )
 		{
-		cells[i].innerHTML=pcSymbol;
-		winChecker(i);
+		cells[i].innerHTML = pcSymbol;
+		winChecker( i );
 		break;
 		}
 	
-}
+	}
 }
