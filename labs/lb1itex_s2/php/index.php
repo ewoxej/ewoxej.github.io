3ab3541ed4e7c5461c199e3e07a35b5e1395e8d1
Illia Chaparyn <illia.chaparyn@nixsolutions.com>
2020-03-28 11:54:47 +0200

    index.php updated

--- labs/lb1itex_s2/php/index.php
+++ labs/lb1itex_s2/php/index.php
@@ -2,47 +2,37 @@
 require "connect_to_db.php";
 
 $dbh=MySQLDatabase::connect("lb1itex","root","");
-$sth_1 = $dbh->prepare("SELECT name ID_Vendors FROM vendors");
-$sth_1->execute();
-  
-$result_1 = $sth_1->fetchAll(PDO::FETCH_ASSOC);
+$sth_1 = $dbh->query("SELECT name ID_Vendors FROM vendors");
 
-$sth_2 = $dbh->prepare("SELECT name ID_Category FROM category");
-$sth_2->execute();
-
-$result_2 = $sth_2->fetchAll(PDO::FETCH_ASSOC);
+$sth_2 = $dbh->query("SELECT name ID_Category FROM category");
 
 ?>
 
 
 <p><h2>Task 1</h2></p>
-<p><b>товары выбранного производителя</b></p>
+<p><b>Товары выбранного производителя</b></p>
 <form action="form1.php" method="get">
 <select name="vendorname">
 	<?php
-    foreach($result_1 as $val){
-      foreach($val as $temp){
-        echo "<option>$temp</option>";
+      foreach($sth_1 as $val){
+        echo "<option>$val[0]</option>";
       }
       print("\n");
-    }
   ?>
-	</select>
+</select>
 <p><input type="submit" value="submit"></p>
 </form>
 
 
 <p><h2>Task 2</h2></p>
-<p><b>товары выбранной категории<b></p>
+<p><b>Товары выбранной категории<b></p>
 <form action="form2.php" method="get">
 	<select name="category_name">
 	<?php
-    foreach($result_2 as $val){
-      foreach($val as $temp){
-        echo "<option>$temp</option>";
+    foreach($sth_2 as $val){
+        echo "<option>$val[0]</option>";
       }
       print("\n");
-    }
   ?>
 	</select>
   <p><input type="submit" value="submit"/></p>
@@ -53,6 +43,6 @@ $result_2 = $sth_2->fetchAll(PDO::FETCH_ASSOC);
 <p><b>товары в выбранном ценовом диапазоне</b></p>
 <form action="form3.php" method="get">
 <p>Min:<input id="lower_bound" type="number" name="lower_bound" required></p>
-<p>Max:<input id="lower_bound" type="number" name="upper_bound" oninput="validate()" required></p>
+<p>Max:<input id="upper_bound" type="number" name="upper_bound" required></p>
 <p><input type="submit" value="submit"></p>
 </form>
